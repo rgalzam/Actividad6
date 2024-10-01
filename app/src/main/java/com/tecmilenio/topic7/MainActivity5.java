39d8239424c46e8502a3fd1a6030f8d42a6f9dbf
Roberto Galicia <rgalzam@gmail.com>
2024-10-01 15:21:56 -0600

    Commit inicial 01/10/2024

---
+++ app/src/main/java/com/tecmilenio/topic7/MainActivity5.java
@@ -0,0 +1,14 @@
+package com.tecmilenio.topic7;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main_5);
+    }
+
+}
